18181bdb80f41e8dcdfbd121934c353e82cb3220
Euan Goodbrand <euangoodbrand@aol.com>
2023-10-15 17:15:42 +0100

    Added class for two-triangles (rectangle mesh)
    I added class for two-triangles (rectangle mesh).

---
+++ TwoTriangles.java
@@ -0,0 +1,19 @@
+public final class TwoTriangles {
+  
+  // ***************************************************
+  /* THE DATA
+   */
+  // anticlockwise/counterclockwise ordering
+  public static final float[] vertices = {      // position, colour, tex coords
+    -0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,  // top left
+    -0.5f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom left
+     0.5f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // bottom right
+     0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f   // top right
+  };
+  
+  public static final int[] indices = {         // Note that we start from 0!
+      0, 1, 2,
+      0, 2, 3
+  };
+
+}
